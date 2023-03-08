ba0af9c0d62ab5ae1335b3c17cc5e204c94d10c8
MacBook Air <macbookair@MacBook.local>
2023-03-08 18:50:25 +0200

    HW0

---
+++ Desktop/js/js0/contacts.js
@@ -0,0 +1,5 @@
+let potato = prompt("Сколько вы продали килограм помидоров за месяц?");
+let mounth = prompt("Сколько дней в месяце?");
+let potatoday = potato / mounth;
+let potatoweek =  potato / 7;
+alert("Вы продали в неделю " + potatoweek + " кг,что в день составило " + potatoday + " кг");
