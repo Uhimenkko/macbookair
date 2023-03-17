08620bac41fd806d8508504aaf8440c7c3beaba9
Uhimenkko <125686764+Uhimenkko@users.noreply.github.com>
2023-03-17 18:10:51 +0200

    Delete Desktop directory

--- Desktop/js/js0/contacts.js
+++
@@ -1,5 +0,0 @@
-let potato = prompt("Сколько вы продали килограм помидоров за месяц?");
-let mounth = prompt("Сколько дней в месяце?");
-let potatoday = potato / mounth;
-let potatoweek =  potato / 7;
-alert("Вы продали в неделю " + potatoweek + " кг,что в день составило " + potatoday + " кг");
